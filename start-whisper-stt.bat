99d96af0707538253bec1d6e8ec83b294809d6f5
Prithwiraj Samanta <prskid1000@gmail.com>
2026-04-07 11:45:40 +0530

    updates

--- start-whisper-stt.bat
+++ start-whisper-stt.bat
@@ -1,3 +1,4 @@
 @echo off
+set CUDA_VISIBLE_DEVICES=-1
 title Whisper STT Server (port 6600)
 "C:\Users\prith\.voicemode-windows\stt-venv\Scripts\faster-whisper-server.exe" Systran/faster-whisper-medium --host 127.0.0.1 --port 6600
